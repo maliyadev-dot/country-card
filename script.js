62899e63dfd3b2777c87cc41a023bcdcbea7248a
Maliya DeGaton <maliya.dev@icloud.com>
2026-09-10 12:50:31 -0400

    set up initial country data fetch and log response

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ updateButton.addEventListener('click', function () {
     const newCountry = countryInput.value;         // read what user typed
     countryNameElement.textContent = newCountry;  // update the <h1>
 });
+
+async function testCountryFetch() {
+    const response = await fetch('https://countries.dev/name/Canada');
+    const data = await response.json();
+    console.log(data);
+}
+
+testCountryFetch();
